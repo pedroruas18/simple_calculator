ca96e2fc9b0a9be3985a2d8ad839aaa616d03fbf
Andre Lamurias <alamurias@lasige.di.fc.ul.pt>
2019-11-15 17:08:05 +0000

    Docstrings example

---
+++ calculator_docstrings.py
@@ -0,0 +1,42 @@
+def sum(n1, n2):
+    """
+    Add one number to the other
+
+    :param n1: First number
+    :type n1: float
+    :param n2: Second number
+    :type n2: float
+    :return: sum of two arguments
+    :rtype: float
+
+    :Example:
+
+    >>> sum(1, 2)
+    3
+    >>> sum(-1, 100)
+    99
+
+    """
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    """
+    Subtract one number to the other
+
+    :param n1: First number
+    :type n1: float
+    :param n2: Second number
+    :type n2: float
+    :return: subtraction of two arguments
+    :rtype: float
+
+    :Example:
+
+    >>> subtract(1, 2)
+    -1
+    >>> subtract(-1, 100)
+    -101
+
+    """
+    return n1 - n2
